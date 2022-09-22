1fb12921bee0c6320960d16ad0513cf91bb7f905
wesh <the_bat_2015@mail.ru>
2022-09-22 17:10:42 +0300

    создана форма,вьюха отзывов и шаблон

--- movies/views.py
+++ movies/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from django.views.generic import ListView, DetailView
 
 from .models import Movie
-
+from .forms import ReviewForm
 
 
 class MoviesView(ListView):
@@ -16,3 +16,14 @@ class MovieDetailView(DetailView):
     """Полное описание фильма"""
     model = Movie
     slug_field = "url"
+
+class AddReview(View):
+    """Отзывы"""
+    def post(self, request, pk):
+        form = ReviewForm(request.POST)
+        movie = Movie.objects.get(id=pk)
+        if form.is_valid():
+            form = form.save(commit=False)  # Вызывая сейв, коммит фолс - хотим приостановить хранение нашей формы и можем внести изменения
+            form.movie = movie
+            form.save()
+        return redirect(movie.get_absolute_url())
